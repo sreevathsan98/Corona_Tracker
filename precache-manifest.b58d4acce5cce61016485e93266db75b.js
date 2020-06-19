b02d7784fcad873a08b447c40677aea142daa455
K <sreevathsan.k@hp.com>
2020-06-19 21:49:20 +0530

    Updates

---
+++ precache-manifest.b58d4acce5cce61016485e93266db75b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db56700f33fad9b917f736842aaf9be",
+    "url": "/Corona_Tracker/index.html"
+  },
+  {
+    "revision": "22add1e51f5d2a956d61",
+    "url": "/Corona_Tracker/static/css/main.bd612e38.chunk.css"
+  },
+  {
+    "revision": "14b1a946bfa465885147",
+    "url": "/Corona_Tracker/static/js/2.a0871ea3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona_Tracker/static/js/2.a0871ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22add1e51f5d2a956d61",
+    "url": "/Corona_Tracker/static/js/main.769ad43e.chunk.js"
+  },
+  {
+    "revision": "875b09046a47ef13b002",
+    "url": "/Corona_Tracker/static/js/runtime-main.0ca786ff.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona_Tracker/static/media/image.d7265326.png"
+  }
+]);
